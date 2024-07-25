191705a3b27831df9981e8c467d0ba9dafba87da
ASAFinYuri <132985019+ASAFinYuri@users.noreply.github.com>
2024-07-25 09:15:16 +0300

    healhy

--- React/asa-react/src/components/Healthy.tsx
+++ React/asa-react/src/components/Healthy.tsx
@@ -5,16 +5,19 @@ import { AsaStateContext} from '../components/asaStateProvider'
 import axios ,{AxiosError} from "axios";
 import {IAsaResponse} from '../services/apiCallService'
 const ACCOUNTTYPE_PATH='Account/AccountTypes'
+const LINKFI_URL='https://linkfi-api-qa.azurewebsites.net/api/'
+//const LINKFI_URL='http://localhost:64245/api/'
 interface Props {
     option?: any
     // any props that come into the component
 }
 
 const callHealthyOptionCheck =async (option:string,arg:any)=>{
-    const { data } = await axios.get('http://localhost:64245/api/HeartBeat/OptionCheck',
+    const { data } = await axios.get(LINKFI_URL+'HeartBeat/OptionCheck',
         {
             params: {
-                option: option
+                option: option,
+                arg:arg
         }
     })
         console.log(data)
@@ -22,7 +25,7 @@ const callHealthyOptionCheck =async (option:string,arg:any)=>{
 
 }
 const callHealthyOption =async ()=>{
-    const { data } = await axios.get('http://localhost:64245/api/HeartBeat/Options')
+    const { data } = await axios.get(LINKFI_URL+'HeartBeat/Options')
         console.log(data)
         return data;
 
@@ -34,7 +37,7 @@ const HealthyOption=({option}:Props)=>{
         const getData=async ()=>{
             try{
                
-                const data=await callHealthyOptionCheck(option.option,undefined)
+                const data=await callHealthyOptionCheck(option.option,option.arg)
                 setoptState(data)
                 console.log(data)
             }
@@ -50,17 +53,21 @@ const HealthyOption=({option}:Props)=>{
     return (
     <Row key={option.option}>
         <Col className='p-3 mb-2 bg-primary text-white'>{option.option}</Col>
-        <Col>{option.arg}</Col>
         <Col sm={4}>{option.description}</Col>
         {loading && 
          <Col>
             <Spinner animation="grow" variant="secondary" />
          </Col>
         }
-        {!loading && optState &&
+        {!loading && optState && optState.status===200 && 
             <Col className='p-3 mb-2 bg-success text-white'>{optState.data}</Col>
         }
-
+        {!loading && optState && optState.status!==200 && optState.status<400 &&
+            <Col className='p-3 mb-2 bg-warning text-dark'>{optState.data}</Col>
+        }
+        {!loading && (!optState || optState.status>=400) && 
+            <Col className='p-3 mb-2 bg-danger text-white'>Error</Col>
+        }
     </Row>
     )
 }
@@ -88,18 +95,18 @@ const Healthy=()=>{
             <p><a href="#" className="text-danger">Error accessing</a></p>
         )
     }
-    if(!options){
+    if(!!!options){
         return (
-            <div>.........</div>
+            <Spinner animation="grow" variant="secondary" />
         )
     }
 
     return (
 
             <Container className='text-start asa-tab-data-in'>
+              
                 <Row className='fw-bold fs-4'>
-                    <Col>option</Col>
-                    <Col>arg</Col>
+                    <Col>service</Col>
                     <Col sm={4}>description</Col>
                     <Col>status</Col>
                 </Row>
